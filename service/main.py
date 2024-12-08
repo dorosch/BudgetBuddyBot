5a5b0b633b0f6f5980213b77dbfc0ef444bc354f
Andrey Doroschenko <andrey.daraschenka@gmail.com>
2024-12-09 00:56:32 +0200

    Added handler for the upload command

--- service/main.py
+++ service/main.py
@@ -3,8 +3,12 @@ import logging.config
 
 from aiogram import Bot, Dispatcher
 from aiogram.exceptions import TelegramRetryAfter
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, Command
+from aiogram.fsm.storage.mongo import MongoStorage
+from aiogram.fsm.state import StatesGroup, State
 from aiogram.handlers import MessageHandler
+from aiogram.types import BotCommand, ReplyKeyboardMarkup, KeyboardButton
+from motor.motor_asyncio import AsyncIOMotorClient
 
 from config import settings
 from database import core as database
@@ -13,7 +17,21 @@ from database.models import User
 logging.config.dictConfig(settings.LOGGING_CONFIG)
 logger = logging.getLogger(__name__)
 
-dispatcher = Dispatcher()
+dispatcher = Dispatcher(
+    storage=MongoStorage(
+        AsyncIOMotorClient(settings.MONGODB_URI, authSource="admin")
+    )
+)
+
+MENU_COMMANDS = [
+    BotCommand(command="/help", description="Information about the bot"),
+    BotCommand(command="/upload", description="Upload your account statement"),
+]
+
+
+class UploadBankAccountStatement(StatesGroup):
+    selected_bank = State()
+    uploaded_file = State()
 
 
 @dispatcher.message(CommandStart())
@@ -32,12 +50,35 @@ class StartCommandHandler(MessageHandler):
         await self.event.answer("Start")
 
 
+@dispatcher.message(Command("upload"))
+class UploadBankAccountStatementHandler(MessageHandler):
+    """Handler of the upload command."""
+
+    reply_markup = ReplyKeyboardMarkup(keyboard=[
+        [
+            KeyboardButton(text="Swedbank", callback_data="Swedbank"),
+            KeyboardButton(text="Revolut", callback_data="Revolut"),
+        ]
+    ])
+
+    async def handle(self):
+        await self.event.answer(
+            "Please choose your bank",
+            reply_markup=self.reply_markup
+        )
+
+        await self.data["state"].set_state(
+            UploadBankAccountStatement.selected_bank
+        )
+
+
 async def main():
     await database.init()
 
     bot = Bot(token=settings.TOKEN)
 
     try:
+        await bot.set_my_commands(MENU_COMMANDS)
         await dispatcher.start_polling(bot)
     finally:
         try:
